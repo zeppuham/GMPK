7a3deb5906cc830cf6d40ac4b7d4544bdda5aca0
zeppuham <zeppuham@dukes.jmu.edu>
2017-03-26 17:12:10 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ if (login_check($mysqli) == true) {
 <html>
 <head>
     <title>Volunteer Log In</title>
-    <link rel="stylesheet" href="styles/main.css" />
+    <link rel="stylesheet" type="text/css" href="styles/login.css" />
     <script type="text/JavaScript" src="js/sha512.js"></script>
     <script type="text/JavaScript" src="js/forms.js"></script>
+    <script src="js/login.js"></script>
 </head>
 <body>
 <?php
@@ -22,17 +23,28 @@ if (isset($_GET['error'])) {
     echo '<p class="error">Error Logging In!</p>';
 }
 ?>
-<h1>Welcome! Please sign in</h1>
-<form action="includes/process_login.php" method="post" name="login_form">
-    Email: <input type="text" name="txtEmail" />
-    Password: <input type="password"
-                     name="txtPassword"
-                     id="password"/>
-    <input type="button"
-           value="Login"
-           onclick="formhash(this.form, this.form.password);" />
-</form>
-<p>Want to be a volunteer? Fill out an application today! </p>
-<p><a href="AnimalCareApp.php">Animal Care Application</a></p>
+<div class="page">
+    <div class="form">
+        <img src="images/text-logo-300.png"><br><br>
+        <form action="includes/process_login.php" method="post" name="login_form" class="login-form">
+
+            <input type="text" name="txtEmail" placeholder="Email" />
+            <input type="password"
+                             name="txtPassword"
+                             id="password"
+                              placeholder="Password"/>
+            <input type="button"
+                   value="Login"
+                   id="profile-form-link"
+                   onclick="formhash(this.form, this.form.password);" /> <br> <br>
+            <input type="button"
+                   value="Register"
+                   id="register-form-link"
+                   onclick="register.php" /> <br> <br>
+            <p class="message">Return to <a href="http://www.wildlifecenter.org">The Wildlife Center</a> website.</p>
+        </form>
+    </div>
+</div>
+
 </body>
 </html>
