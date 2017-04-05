42993116786ded1531a25e815352b6e228648406
slmnet <trae.mazza@gmail.com>
2017-04-05 19:57:59 -0400

    update 4/5

--- Applications/outreach-docent-volunteer-application.php
+++ Applications/outreach-docent-volunteer-application.php
@@ -10,7 +10,16 @@ sec_session_start();
     <title>Outreach Docent Application</title>
     <script type="text/JavaScript" src="../js/sha512.js"></script>
     <script type="text/JavaScript" src="../js/forms.js"></script>
-    <link rel="stylesheet" href="styles/main.css" />
+    <script type="text/JavaScript" src="js/sha512.js"></script>
+    <script type="text/JavaScript" src="js/forms.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../styles/register.css">
+    <script src="js/register.js"></script>
     <style>
         #vacCost {
             display:none;
@@ -19,9 +28,13 @@ sec_session_start();
 </head>
 <body>
 <?php if (login_check($mysqli) == true) : ?>
-    <form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>"
+<div class="page">
+    <div class="form">
+        <center><img src="../images/text-logo-300.png"></center><br><br>
+        <h1>Outreach Volunteer Application</h1>
+    <form action="../includes/Outreach-Docent-Volunteer-Application.inc.php"
           method="post"
-          name="registration_form">
+		  enctype="multipart/form-data">
         <!--
         <label for="txtName">Name: </label><input type='text' name="txtName" id="txtName" /><br>
         <label for="txtAddress">Address: </label><input type="text" name="txtAddress" id="txtAddress" /><br>
@@ -31,37 +44,34 @@ sec_session_start();
         <label for="txtZipCode">Zip Code: </label><input type="text" name="txtZipCode" id="txtZipCode" /> <br>
         <label for="txtPhone">Phone: </label><input type="text" name="txtPhone" id="txtPhone" /><br>
         <label for="txtDob">Date of Birth: </label><input type="date" name="txtDob" id="txtDob" /><br>
+       <label for="txtAllergies">Please list any allergies and/or special needs *</label><br>
+        <textarea name="txtAllergies" class="expReq" style="background-color:#ffffff;width:500px;font-size:12px;border:0px;padding:5px;color:#aea9a9;"></textarea><br>
         -->
-
-        <label for="txtAllergies">Please list any allergies and/or special needs *</label><br>
-        <textarea name="txtAllergies" class="expReq"></textarea><br>
-        Availability * <br>
-        <input type="checkbox" name="Availability" value="Monday">
+		Availability * <br>
+        <input type="checkbox" name="chkAvailability[]" value="Monday">
         Monday<br>
-        <input type="checkbox" name="Availability" value="Tuesday">
+        <input type="checkbox" name="chkAvailability[]" value="Tuesday">
         Tuesday<br>
-        <input type="checkbox" name="Availability" value="Wednesday">
+        <input type="checkbox" name="chkAvailability[]" value="Wednesday">
         Wednesday<br>
-        <input type="checkbox" name="Availability" value="Thursday">
+        <input type="checkbox" name="chkAvailability[]" value="Thursday">
         Thursday<br>
-        <input type="checkbox" name="Availability" value="Friday">
+        <input type="checkbox" name="chkAvailability[]" value="Friday">
         Friday<br>
-        <input type="checkbox" name="Availability" value="Saturday">
+        <input type="checkbox" name="chkAvailability[]" value="Saturday">
         Saturday<br>
-        <input type="checkbox" name="Availability" value="Sunday">
+        <input type="checkbox" name="chkAvailability[]" value="Sunday">
         Sunday<br>
 
-
         <strong>Interests and Experience</strong><br>
-        <label for="txtInterest">Why are you interested in volunteering as an outreach docent? *</label><br>
-        <textarea name="txtInterest" class="expReq"></textarea><br>
-        <label for="txtInterest">Why are you interested in volunteering as an outreach docent? *</label><br>
-        <textarea name="txtInterest" class="expReq"></textarea><br>
-        <label for="txtInterest">Why are you interested in volunteering as an outreach docent? *</label><br>
-        <textarea name="txtInterest" class="expReq"></textarea><br>
-        <label for="txtInterest">Why are you interested in volunteering as an outreach docent? *</label><br>
-        <textarea name="txtInterest" class="expReq"></textarea><br><br>
-
+        <label for="txtInterest1">Why are you interested in volunteering as an outreach docent? *</label><br>
+        <textarea name="txtInterest1" class="expReq" style="background-color:#ffffff;width:500px;font-size:12px;border:0px;padding:5px;color:#aea9a9;"></textarea><br>
+        <label for="txtInterest2">What’s an environmental or wildlife issue you feel passionately about, and why? *</label><br>
+        <textarea name="txtInterest2" class="expReq" style="background-color:#ffffff;width:500px;font-size:12px;border:0px;padding:5px;color:#aea9a9;"></textarea><br>
+        <label for="txtInterest3">Do you have prior experience speaking to the public? Please describe.  *</label><br>
+        <textarea name="txtInterest3" class="expReq" style="background-color:#ffffff;width:500px;font-size:12px;border:0px;padding:5px;color:#aea9a9;"></textarea><br>
+        <label for="txtInterest4">What do you think you’d bring to the outreach volunteer team?  *</label><br>
+        <textarea name="txtInterest4" class="expReq" style="background-color:#ffffff;width:500px;font-size:12px;border:0px;padding:5px;color:#aea9a9;"></textarea><br><br>
 
         <h4>Additional Requirements</h4>
         <h5>In order to be considered for a volunteer position, applicants must submit the following additional documents:</h5>
@@ -69,15 +79,31 @@ sec_session_start();
             <li>Resume or CV: This should include information about your education and relevant work history. </li>
             <li>Letter of Recommendation: The letter should be sent directly from your reference.</li>
         </ul>
-        <input type="file" id="myFile">
-        <input type="file" id="myFile">
+        <input type="file" name="resumeFile" id="resumeFile">
+        <input type="file" name="recommendFile" id="recommendFile">
         <br>
-        <input type="button"
+        <input type="submit"
                value="Send Application"
+               name="btnSend"
+			   class="btn btn-success"
+               style="background-color:#b8c076;font-family: Bitter, sans-serif;text-transform: uppercase;color: #FFFFFF;cursor:pointer;"
                onclick="" />
+		<center><p>Return to your <a href="../protected-page.php">Home Page</a>.</p></center>
+        <center><p>Return to the <a href="../index.php">Login Page</a>.</p></center>
     </form>
-    <p>Return to the <a href="../protected_page.php">Volunteer Opportunities Page</a>.</p>
-    <p>Return to the <a href="../index.php">login page</a>.</p>
+	
+	<?php 
+		if ($_SESSION['showSuccess'] == 1){
+            echo "<script type='text/javascript'>alert('Success submitting your Outreach Docent Volunteer Application!')</script>";
+            $_SESSION['showSuccess'] = 0;
+        }
+		if ($_SESSION['showFailure'] == 1){
+            echo "<script type='text/javascript'>alert('Failure submitting your Outreach Docent Volunteer Application.')</script>";
+            $_SESSION['showFailure'] = 0;
+        }
+    ?>
+    </div>
+</div>
     <script>
         function showQ() {
             if (document.getElementById('ddlRabies').value = 'no') {
